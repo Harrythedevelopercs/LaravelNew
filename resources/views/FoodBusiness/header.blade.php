3b0eb9a0d3ae1f14ebc93c32bcdccdced5bd201a
Harrythedevelopercs <72253270+Harrythedevelopercs@users.noreply.github.com>
2022-08-16 18:18:36 +0500

    sercure to assets

--- resources/views/FoodBusiness/header.blade.php
+++ resources/views/FoodBusiness/header.blade.php
@@ -16,24 +16,24 @@
         <link rel="shortcut icon" href="{{ asset('logo.ico') }}">
 
         <!-- jvectormap -->
-        <link href="{{ secure_asset('assets/plugins/jvectormap/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet">
-        <link href="{{ secure_asset('assets/plugins/fullcalendar/vanillaCalendar.css') }}" rel="stylesheet" type="text/css"  />
+        <link href="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/plugins/fullcalendar/vanillaCalendar.css') }}" rel="stylesheet" type="text/css"  />
         
-        <link href="{{ secure_asset('assets/plugins/morris/morris.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/plugins/morris/morris.css') }}" rel="stylesheet">
 
          <!-- DataTables -->
-         <link href="{{ secure_asset('assets/plugins/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href=" {{ secure_asset('assets/plugins/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+         <link href="{{ asset('assets/plugins/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href=" {{ asset('assets/plugins/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
         <!-- Responsive datatable examples -->
-        <link href="{{ secure_asset('assets/plugins/datatables/responsive.bootstrap4.min.css ') }}" rel="stylesheet" type="text/css" /> 
+        <link href="{{ asset('assets/plugins/datatables/responsive.bootstrap4.min.css ') }}" rel="stylesheet" type="text/css" /> 
 
 
-        <link href="{{ secure_asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
-        <link href="{{ secure_asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css">
-        <link href="{{ secure_asset('assets/css/style.css') }}" rel="stylesheet" type="text/css">
-        <script src="{{ secure_asset('assets/js/jquery.min.js') }}"></script>
+        <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css">
+        <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" integrity="sha512-YUkaLm+KJ5lQXDBdqBqk7EVhJAdxRnVdT2vtCzwPHSweCzyMgYV/tgGF4/dCyqtCC2eCphz0lRQgatGVdfR0ww==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <link href="{{ secure_asset('assets/css/evo-calendar.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('assets/css/evo-calendar.min.css') }}" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css2?family=Jost&family=Poppins&display=swap" rel="stylesheet">
         <style>
             *{
